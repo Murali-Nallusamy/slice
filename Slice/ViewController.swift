a3c8f202fb902d14c434810bc02cf92c23bfe9ac
Murali <murali@xcubelabs.com>
2020-11-22 19:02:47 +0530

    Initial Commit

---
+++ Slice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slice
+//
+//  Created by Murali on 22/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
